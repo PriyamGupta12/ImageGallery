3af116d0f58eaf9765a25888a3f3e3a9e2892e16
Priyam Gupta <priyamguptacode@gmail.com>
2023-10-23 10:48:31 +0530

    bibiibib

--- project/src/App.js
+++ project/src/App.js
@@ -1,6 +1,5 @@
-import logo from './logo.svg';
 import './App.css';
-import ImageGallery from '../components/imagegallery';
+import ImageGallery from './imagegallery';
 function App() {
   return (
     <div className="App">
