4ec0d6894a64b51356dc61d7f841a56e1aa3984a
Priyam Gupta <priyamguptacode@gmail.com>
2023-10-23 11:32:30 +0530

    heyheyheyloverrrrrr

--- project/src/imagegallery.jsx
+++ project/src/imagegallery.jsx
@@ -4,9 +4,15 @@ function ImageGallery(){
     return(
         <div>
             <div className="gallery">
-                <div>
-             <span><img src="Pic1" id="img1" ></img></span>
-             <span><img src="Untitled" id="img1" ></img></span>
+            <div>
+             <span><img src='pic1.png' id='img1'></img></span>
+             <span><img src='untitled.png' id='img1'></img></span>
+             <span><img src='pic1.png' id='img1'></img></span>
+             <span><img src='untitled.png' id='img1'></img></span>
+             <span><img src='pic1.png' id='img1'></img></span>
+             <span><img src='untitled.png' id='img1'></img></span>
+             <span><img src='pic1.png' id='img1'></img></span>
+             <span><img src='untitled.png' id='img1'></img></span>
              </div>
              </div>
         </div>
