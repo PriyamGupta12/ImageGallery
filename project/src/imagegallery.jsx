0df4b2722f5deb1a32129b3b32d013eabb56674d
Priyam Gupta <priyamguptacode@gmail.com>
2023-10-23 11:05:01 +0530

    photuuu

--- project/src/imagegallery.jsx
+++ project/src/imagegallery.jsx
@@ -3,8 +3,12 @@ import React from 'react'
 function ImageGallery(){
     return(
         <div>
+            <div className="gallery">
+                <div>
              <span><img src="Pic1" id="nextBtn" ></img></span>
              <span><img src="Untitled" id="nextBtn" ></img></span>
+             </div>
+             </div>
         </div>
     )
 }
